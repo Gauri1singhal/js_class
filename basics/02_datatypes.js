b96a9694ef91f7a9e456103686e08d1a0c2485b7
Gauri Singhal <122613550+Gauri1singhal@users.noreply.github.com>
2024-08-15 22:00:27 +0530

    Update 02_datatypes.js

--- basics/02_datatypes.js
+++ basics/02_datatypes.js
@@ -4,5 +4,5 @@
 console.log(3+3)
 console.log("Hello world")
 console.log(typeof("Gauri"))
-console.log(typeof (null))
-console.log(typeof(undefined))
+console.log(typeof (null)) //object
+console.log(typeof(undefined)) //undefined
