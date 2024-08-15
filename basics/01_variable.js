d8c2d2a0850e87d56b1196e1ba1fb598c7bb957c
Gauri Singhal <122613550+Gauri1singhal@users.noreply.github.com>
2024-08-15 16:29:20 +0000

    first commit

---
+++ basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId= 144553
+let accountEmail= "gaurisinghal496@gmail.com";
+var accountPass= "1234"
+accountCity= "Jaipur"
+
+//accountId= 2;
+accountEmail= "aggarwalshweta.com"
+accountPass= "23456"
+accountCity= "Delhi"
+console.log(accountId)
+console.table([accountEmail, accountId, accountPass, accountCity])
+let accountState;
---
+++ basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)
+console.log(3+3)
+console.log("Hello world")
+console.log(typeof("Gauri"))
+console.log(typeof (null))
+console.log(typeof(undefined))
