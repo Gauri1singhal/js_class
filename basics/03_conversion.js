33877288e71fadb189d55ce9eaf37bd77a7b5193
Gauri Singhal <122613550+Gauri1singhal@users.noreply.github.com>
2024-08-17 17:17:47 +0000

    first commit

---
+++ basics/03_conversion.js
@@ -0,0 +1,46 @@
+let score= true;
+// console.log(typeof(score))
+
+let valueNumber= Number(score);
+// console.log(typeof (valueNumber))
+// console.log(valueNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 false=>0
+//
+
+let isLoggedIn= "";
+let booleanisLoggedin= Boolean(isLoggedIn)
+// console.log(booleanisLoggedin)
+
+//1=>true; 0=>false
+//""=>false
+//"hitesh"=>true
+
+let num=33
+let stringnum= String(num)
+// console.log(stringnum)
+// console.log(typeof stringnum)
+
+//**********operations************
+
+let value= 3;
+let negval= -value;
+console.log([value, negval])
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1= "hello "
+let str2= "hitesh"
+let str3= str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2") 
