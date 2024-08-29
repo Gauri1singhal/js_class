d962622100d76359071018511115678134c6b983
Gauri Singhal <122613550+Gauri1singhal@users.noreply.github.com>
2024-08-29 17:17:38 +0000

    commit

---
+++ controlFlow04/one.js
@@ -0,0 +1,94 @@
+//if statement
+// if(condition==true){
+// code is executed
+// }
+
+//<,>,<=,>=,==,!=,===(checks the value and even the type of variable)
+// 2<=2 true
+// 3!=2 true
+// 2==="2" false
+
+// const balance=1000
+// if(balance<500){
+//     console.log("<500")
+// }
+// else if(balance>500 && balance<700){
+//     console.log("500-700")
+// }
+// else{
+//     console.log("balance>700")
+// }
+
+//switch
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month=3;
+// switch(month){
+//     case 1: console.log("1");
+//     break;
+//     case 2: console.log("2");
+//     break;
+//     case 3: console.log("3");
+//     break;
+//     case 4: console.log("4");
+//     break;
+//     case 5: console.log("5");
+//     break;
+//     case 6: console.log("6");
+//     break;
+//     case 7: console.log("7");
+//     break;
+//     case 8: console.log("8");
+//     break;
+//     case 9: console.log("9");
+//     break;
+//     case 10: console.log("10");
+//     break;
+//     case 11: console.log("11");
+//     break;
+//     case 12: console.log("12");
+//     break;
+//     default: console.log("not appropriate")
+// }
+
+// const username="hitesh@gmail.com"
+// if(username){
+//     console.log("Got user email")
+// }
+// else{
+//     console.log("Dont have user email")
+// }
+
+//falsy value
+//false, 0, -0, BignInt 0n, null, "", undefined, NaN
+
+//truthsy value
+//true, int , char, string,"0", 'false', " ", [], {}, function(){}
+
+// const emptyobj= {}
+// if(Object.keys(emptyobj).length===0){
+//     console.log("Object is empty")
+// }
+
+ //Nullish coalescing operator (??): null undefined
+
+//  let val1;
+//  val1= 5??10
+// val1= null??10
+//  val1= undefined?? 15
+// val1= null?? 10?? 20
+//  console.log(val1)
+
+//ternary operator
+// condition? true:false
+
+// const price=100
+// price>80 ? console.log("Yes"): console.log("No")
+// console.log(price)
