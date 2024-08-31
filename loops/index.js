6486290b1d768112fb51eb23343d242c89b819fa
Gauri Singhal <122613550+Gauri1singhal@users.noreply.github.com>
2024-08-31 18:03:43 +0000

    commit

---
+++ loops/index.js
@@ -0,0 +1,44 @@
+//for loop
+// for(let index=0; index<10; index++){
+//     const element= index;
+//     console.log(element);
+// }
+
+// for(let i=0; i<=10; i++){
+//     for(let j=0; j<20; j++){
+//         console.log(i,j); 
+//     }
+// }
+
+//const arr= [1,2,3,4,5,6]
+// const arr=["flash", "superman", "heroes"]
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element)
+// }
+
+//break and continue
+// for(let i=0;i<10; i++){
+    
+//     if(i==5){
+//         console.log("5 is found")
+//         break;
+//     }
+//     if(i==9){
+//         console.log("9 is found")
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+//while loop
+// let i=10
+// while(i){
+//     console.log(i)
+//     i--;
+// }
+let i=0
+do{
+    console.log(i)
+    i++;
+}while(i<10)
